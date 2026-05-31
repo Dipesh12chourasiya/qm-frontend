a4a87b2e50cadb92d56d4d8f9354157cf2ce25bd
Dipesh <dipeshchourasiya12@gmail.com>
2026-05-31 13:31:29 +0530

    dd

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://qm-backend-qf6q2p0o2-dipeshs-projects-539f827b.vercel.app/",
+  baseURL: "https://qm-backend-qf6q2p0o2-dipeshs-projects-539f827b.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
